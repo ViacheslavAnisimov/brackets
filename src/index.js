99f694916f6b75270e4ea3bfc99eb8667ffd9cfa
ViacheslavAnisimov <forvsn@gmail.com>
2023-08-09 03:39:14 +0700

    feat: solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,53 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+// function check(str, bracketsConfig) {
+if (str.length % 2 !== 0) return false;
+
+for (let i = 0; i < bracketsConfig.length;) {
+  if (str.includes(bracketsConfig[i].join(''))) {
+    str = str.replace(bracketsConfig[i].join(''), '');
+    i = 0;
+  } else {
+    i++;
+  }
 }
+
+if (str.length === 0) {
+  return true;
+} else {
+  return false;
+}
+}
+
+// module.exports = function check(str, bracketsConfig) {
+  // function check(str, bracketsConfig) {
+//   let arr = str.split('');
+//   if (str.length % 2 !== 0) return false;
+//   for (let i = 0; i < bracketsConfig.length; i++) {
+
+//     for (let j = 0; j < arr.length;) {
+//       // console.log(bracketsConfig[j][0]);
+//       // console.log(str[j]);
+//       if (bracketsConfig[i][0] === arr[j] && bracketsConfig[i][1] === arr[j + 1]) {
+//       // if (str.includes(bracketsConfig[i].join(''))) {
+//         arr.splice(j, 2);
+//         j = 0;
+//       } else {
+//         j++;
+//       }
+//     }
+//   }
+//   console.log(arr);
+//   if (str.length === 0) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// let bracketsConfig = [['(', ')'], ['[', ']'], ['{', '}']];
+// let str = '([{}])';
+
+// console.log(check(str, bracketsConfig));
+
+// console.log(bracketsConfig[0][1]);
+
